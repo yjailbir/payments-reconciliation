21173d933cc3159f1273e3e239f9941ae4a083a8
yjailbir <nkudashko@mail.ru>
2026-05-12 18:46:52 +0300

    Фикс 7

--- core/src/main/java/com/yjailbir/core/config/CorsConfig.java
+++ core/src/main/java/com/yjailbir/core/config/CorsConfig.java
@@ -9,11 +9,9 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("*")
+        registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
