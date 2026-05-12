1a9fd874a18f7bc97dbefd78f1ab14a86312c52e
yjailbir <nkudashko@mail.ru>
2026-05-12 19:20:34 +0300

    Фикс 9

--- core/src/main/java/com/yjailbir/core/config/WebSocketConfig.java
+++ core/src/main/java/com/yjailbir/core/config/WebSocketConfig.java
@@ -1,7 +1,10 @@
 package com.yjailbir.core.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -24,6 +27,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .setSystemHeartbeatReceiveInterval(4000);
         // Префикс для сообщений от клиента (не используется)
         registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(2);
+        scheduler.setThreadNamePrefix("ws-heartbeat-");
+        return scheduler;
     }
 
     @Override
